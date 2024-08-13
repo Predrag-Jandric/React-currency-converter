27c70a2c97c495d57c137ac07e99ae35ca474f33
predrag jandric <predrag.jandric.bg@gmail.com>
2024-08-13 02:08:07 +0200

    ADDED: fetching API logic

--- src/App.js
+++ src/App.js
@@ -5,22 +5,63 @@ import { useState, useEffect } from "react";
 // DOCS https://www.frankfurter.app/docs/
 
 export default function App() {
+  const [amount, setAmount] = useState(0);
+  const [convertFrom, setConvertFrom] = useState("USD");
+  const [convertTo, setConvertTo] = useState("EUR");
+  const [output, setOutput] = useState(null);
+
+  function handleAmount(e) {
+    const parsedValue = parseFloat(e.target.value);
+
+    if (parsedValue >= 0) {
+      setAmount(parsedValue);
+    }
+  }
+
+  function handleconvertFrom(e) {
+    setConvertFrom(() => e.target.value);
+  }
+
+  function handleconvertTo(e) {
+    setConvertTo(() => e.target.value);
+  }
+
+  useEffect(() => {
+    const fetchConvertionRate = async () => {
+      try {
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${convertFrom}&to=${convertTo}`
+        );
+        const data = await res.json();
+        setOutput(data.rates[convertTo]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchConvertionRate();
+  }, [amount, convertFrom, convertTo]);
+
+  console.log(amount);
+  console.log(`i am convert from ${convertFrom}`);
+  console.log(`i am convert to ${convertTo}`);
+
   return (
     <div>
-      <input type="text" />
-      <select>
+      <input type="number" value={amount} onChange={handleAmount} />
+      <select value={convertFrom} onChange={handleconvertFrom}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select value={convertTo} onChange={handleconvertTo}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>OUTPUT</p>
+      <p>{output}</p>
     </div>
   );
 }
